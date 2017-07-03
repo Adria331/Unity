65acbb745d643b7fe4705f2ae0d504568020e454
Adrià Bonet Vidal <Adria331@users.noreply.github.com>
2017-07-03 17:01:41 +0200

    Add files via upload

---
+++ Mob.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Mob : MonoBehaviour {
+
+    public GameObject[] punts = new GameObject[2];
+    public GameObject yo;
+    GameObject actual;
+    int i = 0;
+    public float velo;
+	// Use this for initialization
+	void Start () {
+        actual = punts[0];
+	}
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (transform.position.x == punts[punts.Length-1].transform.position.x)
+            if (transform.position.z == punts[punts.Length-1].transform.position.z)
+            {
+                Debug.LogWarning("ODAAAAAAA");
+                Destroy(yo);
+            }
+                
+
+        if (transform.position == actual.transform.position)
+        {
+            actual = punts[i];
+            if (i + 1 != punts.Length)
+            {
+                i++;
+            }
+        }
+        else
+        {
+            transform.position = Vector3.MoveTowards(transform.position, actual.transform.position, velo);
+        }
+    }
+}
---
+++ spawn.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class spawn : MonoBehaviour {
+
+    Mob script;
+    private int seconds = 1;
+    private float time = 0;
+    public GameObject MobPrefab;
+    public GameObject[] punts = new GameObject[2];
+
+    // Use this for initialization
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (time >= seconds)
+        {
+            createEnemy();
+            seconds++;
+        }
+        time += Time.deltaTime;
+    }
+
+    void createEnemy()
+    {
+        GameObject enemy = Instantiate(MobPrefab, transform.position, transform.rotation) as GameObject;
+        enemy.GetComponent<Mob>().punts = this.punts;
+        enemy.GetComponent<Mob>().yo = enemy;
+        enemy.GetComponent<Mob>().velo = 1;
+    }
+}
